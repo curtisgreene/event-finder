3c82a93b6044f798b986d856ab307b987cf38373
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-17 18:08:29 -0400

    Rest of day one changes (outside of app folder)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :events
+
+  get '/login', to: 'sessions#new', as: :login
+  post '/sessions', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+  post '/appointments/:id', to: 'appointments#create'
 end
